8c0076736336ec6484571d3856ae7cd2ea7b450d
ElMihael666 <ahmadeev.mihael666@yandex.ru>
2024-04-02 18:01:12 +0500

    Name and validate

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ form.innerHTML = `
             margin: 5px;
         }
     </style>
-    <form class="output">
-        <h3>Введите фамилию</h3>
-        <input type="text" placeholder="Введите фамилию здесь...">
+    <form class="output" autocomplete="off">
+        <h3>Фамилия</h3>
+        <input oninput="checkInputIsCyrillic(this, false)" type="text" placeholder="Введите здесь...">
         
-        <h3>Введите имя</h3>
-        <input type="text" placeholder="Введите имя здесь...">
+        <h3>Имя и отчество</h3>
+        <input oninput="checkInputIsCyrillic(this, true)" type="text" placeholder="Введите здесь...">
         
-        <h3>Введите логин</h3>
-        <input type="text" placeholder="Введите логин здесь...">
+        <h3>Логин</h3>
+        <input oninput="checkInputIsLatin(this)" type="text" placeholder="Введите здесь...">
         
         <button type="button">Отправить</button>
     </form>
@@ -32,9 +32,28 @@ form.addEventListener('click', (event) => {
         const textLogin = form.getElementsByTagName('input')[2];
 
         console.log(
-            `${textLogin.value.trim()},${textLogin.value[0]}${textLogin.value.slice(-2).toLowerCase()}CZN86,${textFirstName.value},${textLastName.value},${textLogin.value[0]}${textLogin.value.slice(-2).toLowerCase()}CZN86,${textLogin.value[0]}${textLogin.value.slice(-2).toLowerCase()}186czn,1`);
+            `${textLogin.value},${textLogin.value[0]}${textLogin.value.slice(-2).toLowerCase()}CZN86,${textFirstName.value},${textLastName.value},${textLogin.value[0]}${textLogin.value.slice(-2).toLowerCase()}CZN86,${textLogin.value[0]}${textLogin.value.slice(-2).toLowerCase()}186czn,1`);
         textLastName.value = '';
         textFirstName.value = '';
         textLogin.value = '';
     }
 });
+
+function checkInputIsLatin(input) {
+    const value = input.value.trim();
+    if (value.length === 0) {
+        alert('Поле не может быть пустым.');
+        input.value = '';
+    } else if (!/^[a-zA-Z]+$/.test(value)) {
+        alert('В поле Логин используйте только латиницу без пробелов и спецсимволов.');
+        input.value = '';
+    }
+}
+
+function checkInputIsCyrillic(input, allowSpaces) {
+    const pattern = allowSpaces ? /^[а-яё]+((\s+)|$)/i : /^[а-яё]+$/i;
+    if (pattern.test(input.value) === false) {
+        alert('Введите текст на кириллице.');
+        input.value = '';
+    }
+}
